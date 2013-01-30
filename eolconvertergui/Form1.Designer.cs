b36543c36d14cc43a6bb387a2a1410cbd404b89f
Илья <eixezi@gmail.com>
2013-01-30 14:56:54 +0700

    v. 0.001

---
+++ eolconvertergui/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace eolconvertergui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fileslist = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.windowsrnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.linuxnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearButton = new System.Windows.Forms.ToolStripButton();
+            this.start = new System.Windows.Forms.Button();
+            this.selectFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.selectFile = new System.Windows.Forms.OpenFileDialog();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileslist
+            // 
+            this.fileslist.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.fileslist.Location = new System.Drawing.Point(3, 27);
+            this.fileslist.Name = "fileslist";
+            this.fileslist.Size = new System.Drawing.Size(649, 376);
+            this.fileslist.TabIndex = 0;
+            this.fileslist.UseCompatibleStateImageBehavior = false;
+            this.fileslist.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Имя файла";
+            this.columnHeader1.Width = 95;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Путь";
+            this.columnHeader2.Width = 453;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Статус";
+            this.columnHeader3.Width = 81;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.clearButton,
+            this.toolStripSeparator2,
+            this.toolStripDropDownButton1,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(658, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.windowsrnToolStripMenuItem,
+            this.linuxnToolStripMenuItem});
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(108, 22);
+            this.toolStripDropDownButton1.Text = "Перевод строки";
+            // 
+            // windowsrnToolStripMenuItem
+            // 
+            this.windowsrnToolStripMenuItem.Name = "windowsrnToolStripMenuItem";
+            this.windowsrnToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.windowsrnToolStripMenuItem.Text = "Windows (\\r\\n)";
+            this.windowsrnToolStripMenuItem.Click += new System.EventHandler(this.windowsrnToolStripMenuItem_Click);
+            // 
+            // linuxnToolStripMenuItem
+            // 
+            this.linuxnToolStripMenuItem.Checked = true;
+            this.linuxnToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.linuxnToolStripMenuItem.Name = "linuxnToolStripMenuItem";
+            this.linuxnToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.linuxnToolStripMenuItem.Text = "Linux (\\n)";
+            this.linuxnToolStripMenuItem.Click += new System.EventHandler(this.linuxnToolStripMenuItem_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.clearButton.Image = ((System.Drawing.Image)(resources.GetObject("clearButton.Image")));
+            this.clearButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(23, 22);
+            this.clearButton.Text = "Очистить список";
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(556, 409);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(96, 24);
+            this.start.TabIndex = 2;
+            this.start.Text = "Старт";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // selectFolder
+            // 
+            this.selectFolder.Description = "Выберите папку для конвертации:";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "Добавить папку";
+            this.toolStripButton1.Click += new System.EventHandler(this.adddir_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "Добавить файл";
+            this.toolStripButton2.Click += new System.EventHandler(this.addfile_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "О программе";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(658, 441);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.fileslist);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EOL Converter";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView fileslist;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.FolderBrowserDialog selectFolder;
+        private System.Windows.Forms.OpenFileDialog selectFile;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem windowsrnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem linuxnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton clearButton;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+    }
+}
+
---
+++ eolconvertergui/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace eolconvertergui
+{
+
+    public partial class Form1 : Form
+    {
+        List<FileItem> allFileList = new List<FileItem>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool addDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                string[] fileList = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+
+                foreach (string fileItem in fileList)
+                {
+                    addFile(fileItem);
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool addFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                string[] extensions = new string[] { ".php", ".css", ".html", ".htm", ".js", ".txt", ".htaccess", ".gitattributes", ".gitignore", ".xml", ".sql" };
+                if (Array.IndexOf(extensions, Path.GetExtension(path)) >= 0)
+                {
+                    int index = this.fileslist.Items.Add(Path.GetFileName(path)).Index;
+                    this.fileslist.Items[index].SubItems.Add(Path.GetFullPath(path));
+                    this.fileslist.Items[index].SubItems.Add("В очереди");
+                    FileItem fi = new FileItem();
+                    fi.Index = index;
+                    fi.Filename = path;
+                    allFileList.Add(fi);
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private void adddir_Click(object sender, EventArgs e)
+        {
+            if (selectFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                addDirectory(selectFolder.SelectedPath);
+            }
+        }
+
+        private void addfile_Click(object sender, EventArgs e)
+        {
+            if (selectFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                addFile(selectFile.FileName);
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            string endline;
+           
+            if (windowsrnToolStripMenuItem.Checked)
+                endline = "\r\n";
+            else
+                endline = "\n";
+
+            foreach(FileItem fileItem in allFileList) {
+                try
+                {
+                    if (this.fileslist.Items[fileItem.Index].SubItems[2].Text != "OK")
+                    {
+                        StreamReader streamReader = new StreamReader(fileItem.Filename);
+                        string fileContent = "";
+
+                        while (!streamReader.EndOfStream)
+                        {
+                            fileContent += streamReader.ReadLine() + endline;
+                        }
+                        streamReader.Close();
+                        StreamWriter sw = new StreamWriter(fileItem.Filename);
+                        sw.Write(fileContent);
+                        sw.Close();
+                    }
+                    this.fileslist.Items[fileItem.Index].SubItems[2].Text = "OK";
+                }
+                catch
+                {
+                    this.fileslist.Items[fileItem.Index].SubItems[2].Text = "IOError";
+                }
+            }
+        }
+
+        private void windowsrnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            linuxnToolStripMenuItem.Checked = false;
+            windowsrnToolStripMenuItem.Checked = true;
+        }
+
+        private void linuxnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            windowsrnToolStripMenuItem.Checked = false;
+            linuxnToolStripMenuItem.Checked = true;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            allFileList.Clear();
+            this.fileslist.Items.Clear();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("EOL Converter преобразует окончания строк в CRLF или LF.\nСайт автора: http://mobyman.org.\nGithub: https://github.com/mobyman.", "О программе");
+        }
+    }
+
+    public class FileItem
+    {
+        public int Index { get; set; }
+        public string Filename { get; set; }
+    }
+}
